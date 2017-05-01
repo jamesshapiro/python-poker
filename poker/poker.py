45edebc0960b0598d0db844248756519b53d3fa5
James Shapiro <jamesshapiro@gmail.com>
2017-05-01 01:16:41 -0700

    extend simulator to evaluate pocket strength versus any number of opponents

--- poker/poker.py
+++ poker/poker.py
@@ -19,24 +19,30 @@ class Hand:
         for card in self.cards:
             self.suit_count[card.suit] += 1
             self.rank_count[card.rank] += 1
-
         self.suit_counts = {*self.suit_count.values()}
         self.rank_counts = {*self.rank_count.values()}
-        self.ranks = {*self.rank_count.keys()}
-        if len(self.ranks) == 5:
-            if max(self.ranks) - min(self.ranks) == 4 or {14, 2, 3, 4, 5} < self.ranks:
+        self.distinct_ranks = {*self.rank_count.keys()}
+        if len(self.distinct_ranks) == 5:
+            if max(self.distinct_ranks) - min(self.distinct_ranks) == 4:
                 self.is_straight = True
-        self.lst = sorted(self.cards, key=lambda crd: crd.rank, reverse=True)
-        self.lst = sorted(self.lst, key=lambda crd: self.rank_count[crd.rank], reverse=True)
+            elif {14, 2, 3, 4, 5} < self.distinct_ranks:
+                self.is_straight = True
+        self.lst = sorted(self.cards,
+                          key=lambda crd: crd.rank,
+                          reverse=True)
+        self.lst = sorted(self.lst,
+                          key=lambda crd: self.rank_count[crd.rank],
+                          reverse=True)
         self.lst = list(map(lambda crd: crd.rank, self.lst))
-        if {14, 2, 3, 4, 5} < self.ranks:
+        if {14, 2, 3, 4, 5} < self.distinct_ranks:
             self.lst = self.lst[1:] + self.lst[0:1]
         self.is_flush = max(self.suit_counts) == 5
         if self.is_straight and self.is_flush:
             self.value = [9] + self.lst
         elif max(self.rank_counts) == 4:
             self.value = [8] + self.lst
-        elif (self.rank_count.most_common(2)[0][1], self.rank_count.most_common(2)[1][1]) == (3, 2):
+        elif (self.rank_count.most_common(2)[0][1],
+              self.rank_count.most_common(2)[1][1]) == (3, 2):
             self.value = [7] + self.lst
         elif self.is_flush:
             self.value = [6] + self.lst
@@ -44,7 +50,8 @@ class Hand:
             self.value = [5] + self.lst
         elif max(self.rank_counts) == 3:
             self.value = [4] + self.lst
-        elif (self.rank_count.most_common(2)[0][1], self.rank_count.most_common(2)[1][1]) == (2, 2):
+        elif (self.rank_count.most_common(2)[0][1],
+              self.rank_count.most_common(2)[1][1]) == (2, 2):
             self.value = [3] + self.lst
         elif max(self.rank_counts) == 2:
             self.value = [2] + self.lst
@@ -57,29 +64,39 @@ class Hand:
     def __lt__(self, other):
         return self.value < other.value
 
-def evaluate_pocket_heads_up(card1, card2):
+def evaluate_pocket_heads_up(card1,
+                             card2,
+                             n_other_players = 1,
+                             num_simulations = 250):
+    assert(n_other_players > 0)
     deck = Deck()
     deck.remove(card1)
     deck.remove(card2)
     player1 = [card1, card2]
     simulation_tally = [0, 0, 0]
-    num_simulations = 250
     for _ in range(num_simulations):
         deck.shuffle()
         player1 += deck[:5]
-        player2 = deck[:7]
         player1allhands = list(itertools.combinations(player1, 5))
-        player2allhands = list(itertools.combinations(player2, 5))
         combos = list(player1allhands)
         bestComboForP1 = max(combos, key=lambda x: Hand(x).value)
-        combos = list(player2allhands)
-        bestComboForP2 = max(combos, key=lambda x: Hand(x).value)
-        if Hand(bestComboForP1) > Hand(bestComboForP2):
-            simulation_tally[0] += 1
-        elif Hand(bestComboForP1) < Hand(bestComboForP2):
+        is_losing_hand = False
+        is_tying_hand = False
+        for i in range(n_other_players):
+            other_player_i = deck[:5] + deck[5+(2*i):7+(2*i)]
+            allhands = list(itertools.combinations(other_player_i, 5))
+            combos = list(allhands)
+            bestCombo = max(combos, key=lambda x: Hand(x).value)
+            if Hand(bestComboForP1) < Hand(bestCombo):
+                is_losing_hand = True
+            elif Hand(bestComboForP1) == Hand(bestCombo):
+                is_tying_hand = True
+        if is_losing_hand:
             simulation_tally[1] += 1
-        else:
+        elif is_tying_hand:
             simulation_tally[2] += 1
+        else:
+            simulation_tally[0] += 1
         player1 = player1[:2]
     return [round(x/num_simulations, 4) for x in simulation_tally]
     
@@ -113,5 +130,5 @@ print("P2: {}".format(Hand(bestComboForP2).value))
 hand = Hand([Card(rank=12, suit='clubs'), Card(rank=13, suit='hearts'), Card(rank=12, suit='spades'), Card(rank=3, suit='diamonds'), Card(rank=3, suit='spades')])
 
 pocket_aces = [Card(rank=14, suit='spades'), Card(rank=14, suit='clubs')]
-sim = evaluate_pocket_heads_up(*pocket_aces)
+sim = evaluate_pocket_heads_up(*pocket_aces, 3, 250)
 print("Wins: {}, Losses: {}, Ties: {}".format(sim[0], sim[1], sim[2]))
